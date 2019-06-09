d8e3e528ed1a4002df2e638f015ee3492bb68f5c
Dylan Reuter <djreuter45@gmail.com>
2019-06-09 10:22:05 -0500

    finished methods for locations

--- api/v1/locations.go
+++ api/v1/locations.go
@@ -1,16 +1,67 @@
 package v1
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/djackreuter/go-bully-api/models"
-    "net/http"
+	"net/http"
+
+	"github.com/djackreuter/go-bully-api/models"
+	"github.com/gin-gonic/gin"
 )
 
 func GetLocations(c *gin.Context) {
-    res, err := models.GetLocations()
+	res, err := models.GetLocations()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+func GetLocation(c *gin.Context) {
+	res, err := models.GetLocation(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+func AddLocation(c *gin.Context) {
+	var newLoc = models.Location{}
+	if err := c.ShouldBindJSON(&newLoc); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+    res, err := models.AddLocation(&newLoc)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+func UpdateLocation(c *gin.Context) {
+    loc := models.Location{}
+    if err := c.ShouldBindJSON(&loc); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err})
+        return
+    }
+    res, err := models.UpdateLocation(&loc)
     if err != nil {
         c.JSON(http.StatusBadRequest, gin.H{"error": err})
         return
     }
     c.JSON(http.StatusOK, res)
+}
+
+func DeleteLocation(c *gin.Context) {
+    loc := models.Location{}
+    if err := c.ShouldBindJSON(&loc); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err})
+        return
+    }
+    if err := models.DeleteLocation(&loc); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err})
+        return
+    }
+    c.JSON(http.StatusOK, gin.H{"message": "Location deleted successfully"})
 }
--- models/location.go
+++ models/location.go
@@ -1,36 +1,92 @@
 package models
 
 import (
-    "database/sql"
-    "github.com/djackreuter/go-bully-api/db"
-    _ "github.com/go-sql-driver/mysql" 
+	"database/sql"
+    "strconv"
+
+	"github.com/djackreuter/go-bully-api/db"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Location struct {
-    ID   int    `json:"id"`
-    Name string `json:"name"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 var conn *sql.DB
 
 func GetLocations() ([]Location, error) {
-    conn := db.DBConnect()
+	conn := db.DBConnect()
 
-    sqlstatement := "SELECT * FROM locations"
-    rows, err := conn.Query(sqlstatement)
+	sqlstatement := "SELECT * FROM locations;"
+	rows, err := conn.Query(sqlstatement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	result := []Location{}
+
+	for rows.Next() {
+		loc := Location{}
+		if err := rows.Scan(&loc.ID, &loc.Name); err != nil {
+			return nil, err
+		}
+		result = append(result, loc)
+	}
+	return result, nil
+}
+
+func GetLocation(id string) (*Location, error) {
+	conn := db.DBConnect()
+
+	sqlstatement := "SELECT * FROM locations WHERE id = ?;"
+	row := conn.QueryRow(sqlstatement, id)
+	loc := &Location{}
+
+	err := row.Scan(&loc.ID, &loc.Name)
+	if err != nil {
+		return loc, err
+	}
+	return loc, nil
+}
+
+func AddLocation(loc *Location) (*Location, error) {
+	conn := db.DBConnect()
+    var newLoc *Location
+
+    sqlstatement := "INSERT INTO locations (name) VALUES(?);"
+
+    res, err := conn.Exec(sqlstatement, loc.Name)
     if err != nil {
-        return nil, err
+        return newLoc, err
     }
-    defer rows.Close()
-
-    result := []Location{}
-
-    for rows.Next() {
-        loc := Location{}
-        if err := rows.Scan(&loc.ID, &loc.Name); err != nil {
-            return nil, err
-        }
-        result = append(result, loc)
+    intId, err := res.LastInsertId()
+    if err != nil {
+        return newLoc, err
     }
-   return result, nil 
+    return GetLocation(strconv.FormatInt(intId, 16))
+}
+
+func UpdateLocation(loc *Location) (*Location, error) {
+    conn := db.DBConnect()
+    var updatedLoc *Location
+
+    sqlstatement := "UPDATE locations SET name = ? WHERE id = ?;"
+    _, err := conn.Exec(sqlstatement, loc.Name, loc.ID)
+    if err != nil {
+        return updatedLoc, err
+    }
+    return GetLocation(strconv.Itoa(loc.ID))
+}
+
+func DeleteLocation(loc *Location) error {
+    conn := db.DBConnect()
+    sqlstatement := "DELETE FROM locations WHERE id = ?;"
+
+    _, err := conn.Exec(sqlstatement, loc.ID)
+    if err != nil {
+        return err
+    }
+    return nil
 }
